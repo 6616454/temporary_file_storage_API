61ce13a50b1a73c9acb40d85691babdd462411a2
hhssome <artempavel-terekhovy@mail.ru>
2022-07-06 23:06:55 +0300

    added methods for querying and clearing directories

--- do_rar/service.py
+++ do_rar/service.py
@@ -1,3 +1,5 @@
+import os
+
 import aiofiles
 from aiohttp import ClientSession
 from fastapi import UploadFile, Depends, HTTPException
@@ -9,42 +11,16 @@ from zipstream import AioZipStream
 
 from auth.schemas import User
 from database import get_session
-
 import tables
+
 from do_rar.schemas import UpdateFile
 from requests import get_request_session
 from settings import settings
 
-from .tasks import deletion_by_time
+import uuid
+from uuid import UUID
 
-
-# class LinkShortener:
-#     URL = 'https://goo.su/api/links/create'
-#
-#     HEADERS = {
-#         'x-goo-api-token': settings.token_api
-#     }
-#
-#     PAYLOAD = {}
-#
-#     # def __init__(self, request_session: ClientSession = Depends(get_request_session)):
-#     #     self.request_session = request_session
-#
-#     async def get_file_link(self, file_id: int):
-#         link = f'{settings.main_link}{file_id}'
-#         self.PAYLOAD['url'] = link
-#
-#     async def create_short_link(self, file_id: int):
-#         await self.get_file_link(file_id)
-#
-#         async with self.request_session.post(
-#                 url=self.URL,
-#                 headers=self.HEADERS,
-#                 data=self.PAYLOAD
-#         ) as response:
-#             data = await response.json()
-#
-#         return data['short_url']
+from .tasks import deletion_by_time, clear_temp_files
 
 
 class RarService:
@@ -55,30 +31,6 @@ class RarService:
     }
 
     PAYLOAD = {}
-
-    def __init__(self, session: AsyncSession = Depends(get_session),
-                 request_session: ClientSession = Depends(get_request_session)):
-        self.request_session = request_session
-        self.session = session
-
-    async def get_file_link(self, file_id: int):
-        link = f'{settings.main_link}{file_id}'
-        print(link)
-        self.PAYLOAD['url'] = link
-
-    async def create_short_link(self, file_id: int):
-        await self.get_file_link(file_id)
-
-        async with self.request_session.post(
-                url=self.URL,
-                headers=self.HEADERS,
-                data=self.PAYLOAD
-        ) as response:
-            data = await response.json()
-
-        print(data['short_url'])
-
-        return data['short_url']
 
     @staticmethod
     async def dict_files(files: list[UploadFile]) -> list[dict[str, str]]:
@@ -102,7 +54,28 @@ class RarService:
             }
         )
 
-    async def _get_file(self, file_id: int, user_id: int = None) -> tables.File:
+    def __init__(self, session: AsyncSession = Depends(get_session),
+                 request_session: ClientSession = Depends(get_request_session)):
+        self.session = session
+        self.request_session = request_session
+
+    async def get_file_link(self, file_id: UUID):
+        link = f'{settings.main_link}{file_id}'
+        self.PAYLOAD['url'] = link
+
+    async def create_short_link(self, file_id: UUID):
+        await self.get_file_link(file_id)
+
+        async with self.request_session.post(
+                url=self.URL,
+                headers=self.HEADERS,
+                data=self.PAYLOAD
+        ) as response:
+            data = await response.json()
+
+        return data['short_url']
+
+    async def _get_file(self, file_id: UUID, user_id: int = None) -> tables.File:
         if not user_id:
             output_file = await self.session.execute(select(tables.File).filter_by(id=file_id))
         else:
@@ -118,19 +91,17 @@ class RarService:
             detail='The file does not exist or public access to the file is not allowed'
         )
 
-        # HTTPException
-
-    async def _update_file(self, file_id: int, user_id: int, **kwargs):
+    async def _update_file(self, file_id: UUID, user_id: int, **kwargs):
         await self.session.execute(
             update(tables.File).filter_by(id=file_id, user_id=user_id).values(**kwargs)
         )
 
         return await self.session.commit()
 
-    async def _delete_file(self, file_id: int, user_id: int):
-        await self.session.execute(delete(tables.File).filter_by(id=file_id, user_id=user_id))
-
-        return await self.session.commit()
+    # async def _delete_file(self, file_id: UUID, user_id: int):
+    #     await self.session.execute(delete(tables.File).filter_by(id=file_id, user_id=user_id))
+    #
+    #     return await self.session.commit()
 
     async def upload_archive_files(
             self,
@@ -142,34 +113,35 @@ class RarService:
 
         path = f'{user.user_path}/{name_archive}.rar'
 
-        rar_stream = AioZipStream(await self.dict_files(files))
+        temp_files = await self.dict_files(files)
+
+        rar_stream = AioZipStream(temp_files)
         async with aiofiles.open(path, mode='wb') as rar_file:
             async for chunk in rar_stream.stream():
                 await rar_file.write(chunk)
 
         new_file = tables.File(
+            id=uuid.uuid4(),
             name=name_archive,
             file=path,
             link='',
             user_id=user.id,
         )
 
+        new_file.link = await self.create_short_link(new_file.id)
+
         self.session.add(new_file)
         await self.session.commit()
         await self.session.refresh(new_file)
 
-        new_file.link = await self.create_short_link(new_file.id)
-
-        await self.session.commit()
-        await self.session.refresh(new_file)
-
+        clear_temp_files.delay(temp_files)
         deletion_by_time.apply_async((new_file.id,), countdown=15)
 
         return new_file
 
     async def get_archive(
             self,
-            file_id: int,
+            file_id: UUID,
             user: User
     ):
         output_file = await self._get_file(file_id)
@@ -183,7 +155,7 @@ class RarService:
 
     async def update_file_info(
             self,
-            file_id: int,
+            file_id: UUID,
             user_id: int,
             file_info: UpdateFile,
     ):
@@ -200,6 +172,6 @@ class RarService:
 
         await self._update_file(file_id, user_id, **updated_file_data.dict())
 
-    async def delete_file(self, file_id: int, user_id: int):
-        await self._delete_file(file_id, user_id)
-        return Response(status_code=status.HTTP_204_NO_CONTENT)
+    # async def delete_file(self, file_id: UUID, user_id: int):
+    #     await self._delete_file(file_id, user_id)
+    #     return Response(status_code=status.HTTP_204_NO_CONTENT)
--- do_rar/tasks.py
+++ do_rar/tasks.py
@@ -1,4 +1,7 @@
-from sqlalchemy import delete
+import os
+from uuid import UUID
+
+from sqlalchemy import select
 
 from celery_app import app
 from database import async_session
@@ -6,15 +9,28 @@ import tables
 import asyncio
 
 
-async def async_deletion_by_time(file_id: int):
+async def async_deletion_by_time(file_id: UUID):
     session = async_session()
-    await session.execute(delete(tables.File).filter_by(id=file_id))
+
+    file_object = await session.execute(select(tables.File).filter_by(id=file_id))
+
+    file = file_object.scalars().first()
+
+    os.remove(file.file)
+
+    await session.delete(file)
     await session.commit()
     await session.close()
 
 
 @app.task
-def deletion_by_time(file_id: int):
+def deletion_by_time(file_id: UUID):
     # asyncio.run(async_deletion_by_time(file_id))
     loop = asyncio.get_event_loop()
     loop.run_until_complete(async_deletion_by_time(file_id))
+
+
+@app.task
+def clear_temp_files(upload_files: list[dict]):
+    for temp_file in upload_files:
+        os.remove(temp_file['file'])
